0869bc32bfbb28d1db4025e2a054e57ebd61adb5
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-22 11:34:34 -0300

    fix: remove comma form-data

--- script.js
+++ script.js
@@ -38,26 +38,29 @@ const getContent = () => {
   for (let i = 0; i < contents.length; i += 1) {
     contentText += ` ${contents[i].value},`;
   }
+  if (contentText[contentText.length - 1] === ',') {
+    contentText = contentText.slice(0, contentText.length - 1);
+  }
   return contentText;
 };
 
 const createFormData = () => {
   const nameInput = document.getElementById('input-name').value;
   const lastName = document.getElementById('input-lastname').value;
-  const name = `Nome: ${nameInput} ${lastName}`;
-  const email = `Email: ${document.getElementById('input-email').value}`;
-  const house = `Casa: ${document.getElementById('house').value}`;
-  const family = `Família: ${document.querySelector('input[name="family"]:checked').value}`;
+  const name = `<p>Nome: ${nameInput} ${lastName}</p>`;
+  const email = `<p>Email: ${document.getElementById('input-email').value}</p>`;
+  const house = `<p>Casa: ${document.getElementById('house').value}</p>`;
+  const family = `<p>Família: ${document.querySelector('input[name="family"]:checked').value}</p>`;
   // https://stackoverflow.com/questions/9618504/how-to-get-the-selected-radio-button-s-value
   const rate = document.querySelector('input[name="rate"]:checked').value;
-  const avaliacao = `Avaliação: ${rate}`;
-  const obs = `Observações: ${document.getElementById('textarea').value}`;
+  const avaliacao = `<p>Avaliação: ${rate}</p>`;
+  const obs = `<p>Observações: ${document.getElementById('textarea').value}</p>`;
   const createData = document.createElement('form');
   createData.id = 'form-data';
   const main = document.querySelector('main');
-  const materias = `Matérias: ${getContent()}`;
+  const materias = `<p>Matérias: ${getContent()}</p>`;
   main.appendChild(createData);
-  createData.innerHTML = `${name},${email},${house},${family},${avaliacao},${obs}, ${materias}`;
+  createData.innerHTML = `${name}${email}${house}${family}${avaliacao}${obs}${materias}`;
 };
 
 const replaceForms = () => {
